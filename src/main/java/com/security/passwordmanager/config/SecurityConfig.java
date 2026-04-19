9fa69a4bb6ed3caadb7548742fd7fe0ef2b87e65
Damjan Bartol Benjak <95582435+bartol00@users.noreply.github.com>
2026-04-19 20:26:50 +0200

    Fixed miscellaneous small security issues (#4)

--- src/main/java/com/security/passwordmanager/config/SecurityConfig.java
+++ src/main/java/com/security/passwordmanager/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -20,6 +21,11 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .sessionManagement(sm -> sm
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/credentials/**").authenticated()
                         .requestMatchers("/api/deletion/authorized/**").authenticated()

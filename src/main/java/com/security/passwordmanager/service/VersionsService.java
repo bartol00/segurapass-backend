6c102351aae3f69ace305f6c05d9d30e7d214953
Damjan Bartol Benjak <95582435+bartol00@users.noreply.github.com>
2026-07-29 20:38:00 +0200

    Added new versioning (#29)

--- src/main/java/com/security/passwordmanager/service/VersionsService.java
+++ src/main/java/com/security/passwordmanager/service/VersionsService.java
@@ -1,39 +1,24 @@
 package com.security.passwordmanager.service;
 
 import xyz.segurapass.api.versions.VersionInfo;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
-
 @Service
-@Slf4j
 public class VersionsService {
 
-    @Value("${app.latest.version}")
-    private String versionNumber;
+    @Value("${app.app-version}")
+    private String appVersion;
 
-    @Value("${app.latest.description}")
-    private String versionDescription;
-
-    @Value("${app.latest.update-time}")
-    private String versionDate;
-
-    @Value("${app.latest.download-url}")
-    private String downloadUrl;
+    @Value("${app.protocol-version}")
+    private String protocolVersion;
 
     public ResponseEntity<VersionInfo> getLatestVersion() {
         VersionInfo versionInfo = new VersionInfo(
-                versionNumber,
-                versionDescription,
-                downloadUrl,
-                LocalDate.parse(versionDate)
+                appVersion,
+                protocolVersion
         );
-
-        log.info("Get Latest Version - Service");
-
         return ResponseEntity.ok(versionInfo);
     }
 
